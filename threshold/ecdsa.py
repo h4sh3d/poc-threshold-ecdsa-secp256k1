62d85391b4611572a8cf3f4199b974af0288fc16
Joel Gugger <joel.gugger@rdlp.ch>
2017-10-30 13:01:55 +0100

    Fix ecdsa impl

--- threshold/ecdsa.py
+++ threshold/ecdsa.py
@@ -28,7 +28,7 @@ def is_zero(p):
 
 def neg(p):
     x, y = p
-    return x, -y % P
+    return x, -y
 
 def contains_point(p):
     x, y = p
@@ -36,9 +36,9 @@ def contains_point(p):
 
 def point_double(p):
     # print("double")
-    if is_zero(p): return p
+    if is_zero(p): return ZERO
     px, py = p
-    lmbda = ((3 * px * px + a) / utils.inverse_mod(2 * py, P)) % P
+    lmbda = ((3 * px * px + a) * utils.inverse_mod(2 * py, P)) % P
     rx = (lmbda * lmbda - 2 * px) % P
     ry = (lmbda * (px - rx) - py) % P
     return rx, ry
@@ -59,41 +59,40 @@ def point_add(p, q):
     ry = (lmbda * (px - rx) - py) % P
     return rx, ry
 
-def point_add2(point, other):
-    """Add one point to another point."""
-
-    # X9.62 B.3:
-    pointx, pointy = point
-    otherx, othery = other
-
-    if other == ZERO:
-      return point
-    if point == ZERO:
-      return other
-    if pointx == otherx:
-      if (pointy + othery) % P == 0:
-        return ZERO
-      else:
-        return point_double(point)
-
-    p = P
-
-    l = ((othery - pointy) * \
-         utils.inverse_mod(otherx - pointx, p)) % p
-
-    x3 = (l * l - pointx - otherx) % p
-    y3 = (l * (pointx - x3) - pointy) % p
-
-    return x3, y3
-
 def point_mult(p, scalar):
-    r = ZERO
-    d = [int(x) for x in bin(scalar)[2:]]
-    for i in d:
-        if i == 1:
-            r = point_add(r, p)
-        p = point_double(p)
-    return r
+    """Multiply a point by an integer."""
+
+    def leftmost_bit(x):
+      assert x > 0
+      result = 1
+      while result <= x:
+        result = 2 * result
+      return result // 2
+
+    e = scalar
+    if n:
+      e = e % n
+    if e == 0:
+      return ZERO
+    if is_zero(p):
+      return ZERO
+    assert e > 0
+
+    # From X9.62 D.3.2:
+
+    e3 = 3 * e
+    negative_self = neg(p)
+    i = leftmost_bit(e3) // 2
+    result = p
+    while i > 1:
+      result = point_double(result)
+      if (e3 & i) != 0 and (e & i) == 0:
+        result = point_add(result, p)
+      if (e3 & i) == 0 and (e & i) != 0:
+        result = point_add(result, negative_self)
+      i = i // 2
+
+    return result
 
 def key_gen(g):
     d = utils.randomnumber(n-1)
@@ -116,36 +115,38 @@ def verify(sig, e, pub, g, n):
     u1 = e * w % n
     u2 = r * w % n
     x, y = point_add(point_mult(g, u1), point_mult(pub, u2))
-    print(r, s)
-    print(x, y)
     return r == x
 
-if __name__ == "__main__":
-    print("ECDSA")
-
+def test():
     # G x 2313 = 
     res = (0x8199C9D61224F51FE6DCDC333869D86095C0BD8E210D2D7F8FED2804A89AADF9, 0xBE89724F5CBD2384AE9BBD73F030DC74A158EE7D2A9D292DDAAE30574B1EC89B)
     print(contains_point(res))
     # print(point_mult(G, 2313))
-    # print(point_mult(G, 2313) == res)
+    # print(res)
+    print(point_mult(G, 2313) == res)
+
+    # resdouble = (0xFCCC6C53DBB615C0324647998F2C4F3ED47E84E3BDE5A500E025AFF4AECA9C6C, 0x41768DB0A342DC7B5164428C0FCF238B5EA71182D562D6D22551CFA7B4E13394)
+    print(point_add(res, res) == point_double(res))
+
     # G x 78439 =
     res2 = (0xA92DDF0D702330BD535FBB5F9C0EC4E04A4C8427ECA4E33818D663FF0FD2A74E, 0xF9F45EB21C5A2395097EB0336F2683A3662E03E312B7A52BC98E20F105E0FF9)
     print(contains_point(res2))
-    # print(point_mult(G, 78439))
-    # print(point_mult(G, 78439) == res)
+    print(point_mult(G, 78439) == res2)
 
     resadd = (0xB425DFAB6C95A64F0ACE24DAFF4440CE02EEF71EB7127DC4924D0869F10E4C1B, 0x1B16D85927660387414111493AAF0CF0B6B3B83F4F6E5EF9EFBD3CAEBE4F7AF0)
     print(contains_point(resadd))
-    # print(point_add(res, res2))   
-    print(point_add(res, res2) == resadd)
-    print(point_add2(res, res2) == resadd)
+    print(point_add(res, res2) == resadd)    
 
-    # pub, priv = key_gen(G)
-    # message = "Hello world"
-    # h = hashlib.sha256()
-    # h.update(message.encode("utf-8"))
-    # m = long(h.hexdigest(), 16)
-    # sig = sign(m, G, n, priv)
-    # print(sig)
-    # print(verify(sig, m, pub, G, n))
-    # print(point_double(G) == double_g)
+if __name__ == "__main__":
+    print("ECDSA")
+    # test()
+
+    pub, priv = key_gen(G)
+    message = "Hello world"
+    h = hashlib.sha256()
+    h.update(message.encode("utf-8"))
+    m = long(h.hexdigest(), 16)
+    sig = sign(m, G, n, priv)
+    print(sig)
+    print(verify(sig, m, pub, G, n))
+    print(point_double(G) == double_g)
